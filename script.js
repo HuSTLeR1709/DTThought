729920b9dd3509459df3c7ddf510e906e27a12a2
HuSTLeR1709 <nabhjeetkatiyar3@gmail.com>
2024-06-23 21:19:45 +0530

    first

---
+++ script.js
@@ -0,0 +1,21 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const backIcon = document.querySelector('.fa-arrow-circle-left');
+    const closeIcon = document.querySelector('.close');
+    const sideMenu = document.querySelector('.side-menu');
+    const leftTop = document.querySelector('.left-top');
+    const closeTop = document.querySelector('.close-top');
+
+    backIcon.addEventListener('click', function() {
+        sideMenu.classList.add('open');
+        leftTop.classList.add('open');
+        closeTop.classList.add('open');
+        backIcon.style.display = 'none';
+    });
+
+    closeIcon.addEventListener('click', function() {
+        sideMenu.classList.remove('open');
+        leftTop.classList.remove('open');
+        closeTop.classList.remove('open');
+        backIcon.style.display = 'flex';
+    });
+});
